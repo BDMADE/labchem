905af62bbcf4b474e397e3341b974299c9ea715c
hmtanbir <tanbir2025@gmail.com>
2016-02-13 17:37:03 +0600

    supplier scaffold created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213113216) do
+ActiveRecord::Schema.define(version: 20160213113629) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -77,6 +77,13 @@ ActiveRecord::Schema.define(version: 20160213113216) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
